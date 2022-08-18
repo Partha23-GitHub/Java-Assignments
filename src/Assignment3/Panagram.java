72fb8c70aa4e2311547a5a7ba9ac2a26292da0c5
partha <parthamanna.bit23@gmail.com>
2022-08-18 22:16:07 +0530

    Panagram logic changed

--- src/Assignment3/Panagram.java
+++ src/Assignment3/Panagram.java
@@ -6,16 +6,13 @@ public class Panagram {
     public static void main(String[] args) {
         Scanner input=new Scanner(System.in);
         String str=input.nextLine();
-        Set<Character>set=new HashSet<>();  //taking set for O(1) searching operation
-        for(char ele='a';ele<='z';ele++){
-            set.add(ele);
-        }
+        Set<Character>set=new HashSet<>();
         System.out.println(isPanagram(str.toLowerCase(),set));
     }
     static boolean isPanagram(String str,Set<Character>set){
         for(int i=0;i<str.length();i++){
-            set.remove(str.charAt(i));
+            set.add(str.charAt(i));
         }
-        return set.size()==0;
+        return set.size()==26;
     }
 }
